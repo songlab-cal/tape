53ad10a23e6b7cb5b3e4e5b6c4a3dadac02070b0
Roshan Rao <roshan_rao@berkeley.edu>
2019-09-10 11:21:10 -0700

    move base models to different file

--- tape_pytorch/models/task_models.py
+++ tape_pytorch/models/task_models.py
@@ -1,38 +1,11 @@
 import torch.nn as nn
 import torch.nn.functional as F
-from pytorch_transformers.modeling_bert import BertConfig
 from pytorch_transformers.modeling_bert import BertOnlyMLMHead
-from pytorch_transformers.modeling_bert import BertModel
 from pytorch_transformers.modeling_bert import BertLayerNorm
 from pytorch_transformers.modeling_utils import PreTrainedModel
 from torch.nn.utils.weight_norm import weight_norm
 
 from tape_pytorch.registry import registry
-
-
-TAPEConfig = BertConfig
-Transformer = BertModel
-registry.register_model('transformer')(Transformer)
-
-
-@registry.register_model('resnet')
-class ResNet(nn.Module):
-    pass
-
-
-@registry.register_model('lstm')
-class LSTM(nn.Module):
-    pass
-
-
-@registry.register_model('unirep')
-class UniRep(nn.Module):
-    pass
-
-
-@registry.register_model('bepler')
-class Bepler(nn.Module):
-    pass
 
 
 @registry.register_task_model('pfam')
