3bbb8bfde436e4ffca908458ceea2fb67dbb28d4
Roshan Rao <roshan_rao@berkeley.edu>
2019-10-03 16:56:10 -0700

    update the download data script

--- download_data_aws.sh
+++ download_data_aws.sh
@@ -2,7 +2,7 @@ mkdir -p ./data
 
 # Download pfam
 while true; do
-     read -p "Do you wish to download and unzip the pretraining corpus? It is 7.7GB compressed and 19GB uncompressed? [y/n]" yn
+     read -p "Do you wish to download and unzip the pretraining corpus? It is 7.7GB compressed and 19GB uncompressed. [y/n]" yn
      case $yn in
 	    [Yy]* ) aws s3 cp s3://proteindata/data_pytorch/pfam.tar.gz .; tar -xzf pfam.tar.gz -C ./data; rm pfam.tar.gz; break;;
             [Nn]* ) exit;;
@@ -10,28 +10,24 @@ while true; do
     esac
 done
 
-# Download Vocab/Model files
-aws s3 cp s3://proteindata/data_pytorch/pfam.model .
-aws s3 cp s3://proteindata/data_pytorch/pfam.vocab .
-
-mv pfam.model data
-mv pfam.vocab data
+echo "Downloading BPE Vocab/Model files"
+aws s3 cp s3://proteindata/data_pytorch/pfam.model . && mv pfam.model data
+aws s3 cp s3://proteindata/data_pytorch/pfam.vocab . && mv pfam.vocab data
 
 # Download Data Files
-aws s3 cp s3://proteindata/data_pytorch/secondary_structure.tar.gz .
-aws s3 cp s3://proteindata/data_pytorch/proteinnet.tar.gz .
-aws s3 cp s3://proteindata/data_pytorch/remote_homology.tar.gz .
-aws s3 cp s3://proteindata/data_pytorch/fluorescence.tar.gz .
-aws s3 cp s3://proteindata/data_pytorch/stability.tar.gz .
-
-tar -xzf secondary_structure.tar.gz -C ./data
-tar -xzf proteinnet.tar.gz -C ./data
-tar -xzf remote_homology.tar.gz -C ./data
-tar -xzf fluorescence.tar.gz -C ./data
-tar -xzf stability.tar.gz -C ./data
-
-rm secondary_structure.tar.gz
-rm proteinnet.tar.gz
-rm remote_homology.tar.gz
-rm fluorescence.tar.gz
-rm stability.tar.gz
+echo "Download TAPE task datasets"
+aws s3 cp s3://proteindata/data_pytorch/secondary_structure.tar.gz . \
+    && tar -xzf secondary_structure.tar.gz -C ./data \
+    && rm secondary_structure.tar.gz
+aws s3 cp s3://proteindata/data_pytorch/proteinnet.tar.gz . \
+    && tar -xzf proteinnet.tar.gz -C ./data \
+    && rm proteinnet.tar.gz
+aws s3 cp s3://proteindata/data_pytorch/remote_homology.tar.gz . \
+    && tar -xzf remote_homology.tar.gz -C ./data \
+    && rm remote_homology.tar.gz
+aws s3 cp s3://proteindata/data_pytorch/fluorescence.tar.gz . \
+    && tar -xzf fluorescence.tar.gz -C ./data \
+    && rm fluorescence.tar.gz
+aws s3 cp s3://proteindata/data_pytorch/stability.tar.gz . \
+    && tar -xzf stability.tar.gz -C ./data \
+    && rm stability.tar.gz
