f7f8d4188e808e48db115eec253cd8c96c05957b
Roshan Rao <roshan_rao@berkeley.edu>
2019-11-15 12:26:27 -0800

    have cleanup_results.sh check for any pytorch_model* rather than just pytorch_model_0

--- cleanup_results.sh
+++ cleanup_results.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for f in results/*; do
-    if [[ ! -d "$f/pytorch_model_0" ]]; then
+    if [[ ! `compgen -G "$f/pytorch_model_*"` ]]; then
         echo rm -rf $f
         rm -rf $f
         echo rm -rf "logs/$(basename $f)"
