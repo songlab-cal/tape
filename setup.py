dcd1644dc43dc23294afd1a8800379d43197e4f4
Roshan Rao <roshan_rao@berkeley.edu>
2019-12-07 15:24:34 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as rf:
 setup(
     name='tape',
     version='0.1',
-    description='Protein Benchmarking Repository',
+    description='Repostory of Protein Benchmarking & Modeling',
     long_description=README,
     author='Roshan Rao, Nick Bhattacharya, Neil Thomas',
     author_email='roshan_rao@berkeley.edu, nickbhat@berkeley.edu, nthomas@berkeley.edu',
@@ -24,7 +24,6 @@ setup(
             'tape-train-distributed = tape.main:run_train_distributed',
             'tape-eval = tape.main:run_eval',
             'tape-embed = tape.main:run_embed',
-            'tape-gridsearch = tape.main:run_gridsearch',
         ]
     },
 )
