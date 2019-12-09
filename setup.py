38c11fc56888d40bde078774431a4a7faa7b0eb3
Roshan Rao <roshan_rao@berkeley.edu>
2019-12-08 22:42:53 -0800

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE', 'r') as lf:
 setup(
     name='tape_proteins',
     packages=find_packages(),
-    version='0.1.2',
+    version='0.2',
     description="Repostory of Protein Benchmarking and Modeling",
     author="Roshan Rao, Nick Bhattacharya, Neil Thomas",
     author_email='roshan_rao@berkeley.edu, nickbhat@berkeley.edu, nthomas@berkeley.edu',
